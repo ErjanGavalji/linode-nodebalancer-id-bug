6c24399cfc1f24da64761f279dfacd1870795c2e
Erjan Gavalji <erjan.gavalji@gmail.com>
2025-03-20 18:33:39 +0200

    Add progress to the reset script

--- reset.sh
+++ reset.sh
@@ -12,16 +12,33 @@ if [ ! -f "${kubeConfig}" ]; then
 	exit 1
 fi
 
+function show_progress() {
+	total=$1
+	if [ -z "${total}" ]; then
+		total=10
+	fi
+	time=$2
+	if [ -z "${time}" ]; then
+		time=3
+	fi
+	for i in $(seq 1 ${total}); do
+		dots=$(printf '.%.0s' $(seq 1 $i))
+		echo -ne "\r${dots} ${i}/${total}"
+		sleep ${time}
+	done
+	echo ""
+}
+
 #==============================================================================
 
 
 export KUBECONFIG=${kubeConfig}
 kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.17.0/cert-manager.yaml
-sleep 60
+show_progress 60
 kubectl delete -f ./app.yml
-sleep 60
+show_progress 60
 kubectl delete namespace cert-manager
-sleep 15
+show_progress 15
 kubectl delete namespace nginx-ingress
-sleep 15
+show_progress 15
 kubectl delete namespace main-app
